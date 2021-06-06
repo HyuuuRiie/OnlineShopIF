48e5fa3a9cbb488427001a2d5df2047a0c6ade2c
Hyuu_riii <58158754+NoobMaster69-stack@users.noreply.github.com>
2021-06-06 16:44:13 +0700

    Update kaki.php

--- OnlineShopIF/kaki.php
+++ OnlineShopIF/kaki.php
@@ -1,7 +1,7 @@
 <div class="container6">
   <div class="grid">
     <div class="dh12">
-      Copyright &copy; Eka Praja Wiyata Mandala, 2019	
+      Copyright &copy; Wahyu Septiadi Fajri, 2019	
 	</div>
   </div>
 </div>
